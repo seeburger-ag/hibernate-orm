fd182a5c69bf4ff86c275e857c7bc9605c133975
Rico Neubauer <R.Neubauer@seeburger.de>
2019-07-11 15:01:07 +0200

    SEE fixing ClassLoader retrieval for JBOSGi

--- hibernate-osgi/src/main/java/org/hibernate/osgi/HibernateBundleActivator.java
+++ hibernate-osgi/src/main/java/org/hibernate/osgi/HibernateBundleActivator.java
@@ -27,6 +27,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.internal.util.ClassLoaderHelper;
 import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
@@ -41,7 +42,7 @@ import java.util.Hashtable;
 /**
  * This BundleActivator provides three different uses of Hibernate in OSGi
  * environments:
- * 
+ *
  * 1.) Enterprise OSGi JPA: The OSGi container/runtime is in charge of managing
  *     all of the client bundles' persistence units.  The container/runtime is
  *     also in charge of creating and managing the EntityManagerFactory through a
@@ -55,29 +56,43 @@ import java.util.Hashtable;
  *     rather than direct use of Configuration, is necessary to shield users
  *     from ClassLoader issues.  See {@link OsgiSessionFactoryService} for more
  *     information.
- * 
+ *
  * @author Brett Meyer
  * @author Tim Ward
  */
 @SuppressWarnings("UnusedDeclaration")
 public class HibernateBundleActivator implements BundleActivator {
-	
+
 	private OsgiClassLoader osgiClassLoader;
 	private OsgiServiceUtil osgiServiceUtil;
-	
+
 	private ServiceRegistration<?> persistenceProviderService;
 	private ServiceRegistration<?> sessionFactoryService;
-	
+
 	@Override
 	@SuppressWarnings("unchecked")
 	public void start(BundleContext context) throws Exception {
 		// build a ClassLoader that uses all the necessary OSGi bundles, and place it into
 		// a well-known location so internals can access it
 		osgiClassLoader = new OsgiClassLoader();
-		osgiClassLoader.addBundle( FrameworkUtil.getBundle( Session.class ) );
-		osgiClassLoader.addBundle( FrameworkUtil.getBundle( HibernatePersistenceProvider.class ) );
+		Bundle hibernateCoreBundle = FrameworkUtil.getBundle( Session.class );
+		Bundle hibernateEntityManagerBundle = FrameworkUtil.getBundle( HibernatePersistenceProvider.class );
+		if (hibernateCoreBundle == null) {
+		    // in JBOSGi hibernate is not deployed as bundle, but as JBoss-module
+		    osgiClassLoader.addClassLoader(Session.class.getClassLoader());
+		}
+		else {
+		    osgiClassLoader.addBundle( hibernateCoreBundle );
+		}
+		if (hibernateEntityManagerBundle == null) {
+		    // in JBOSGi hibernate is not deployed as bundle, but as JBoss-module
+            osgiClassLoader.addClassLoader(HibernatePersistenceProvider.class.getClassLoader());
+		}
+		else {
+		    osgiClassLoader.addBundle( hibernateEntityManagerBundle );
+		}
 		ClassLoaderHelper.overridenClassLoader = osgiClassLoader;
-		
+
 		osgiServiceUtil = new OsgiServiceUtil( context );
 
 		// Build a JtaPlatform specific for this OSGi context
@@ -112,7 +127,7 @@ public class HibernateBundleActivator implements BundleActivator {
 		osgiClassLoader = null;
 		osgiServiceUtil.stop();
 		osgiServiceUtil = null;
-		
+
 		persistenceProviderService.unregister();
 		persistenceProviderService = null;
 		sessionFactoryService.unregister();
